cc1d21fa0beaac034a78dbda42438e42401e2bd7
poojakrp777 <poojakrp777@gmail.com>
2023-01-22 12:31:19 +0530

    [POOJA] Welcome to line comparison project

---
+++ LineComparisonProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison project");
+        }
+    }
+}
