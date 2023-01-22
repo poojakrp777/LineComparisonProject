42b4a4be47e0db12830cfb0efcc1e1b58bee13dc
poojakrp777 <poojakrp777@gmail.com>
2023-01-22 12:37:23 +0530

    [POOJA] Calculating the length of a line

--- LineComparisonProject/Program.cs
+++ LineComparisonProject/Program.cs
@@ -11,6 +11,24 @@ namespace LineComparisonProject
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparison project");
+            Console.WriteLine("Enter Value of : X1");
+            //string value converted to integer
+            int X1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : X2");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y1");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y2");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            //using double to get square root value in double decimal points
+            double length = Math.Sqrt((Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)));
+            Console.WriteLine("Length of the line is :" + length);
+
+            Console.ReadLine();
         }
     }
 }
