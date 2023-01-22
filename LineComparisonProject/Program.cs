88f80ab755d8a8502b87900fce192be0c4d110a6
poojakrp777 <poojakrp777@gmail.com>
2023-01-22 12:44:11 +0530

    [POOJA] Calculating the two lines length and compare length are equal or not

--- LineComparisonProject/Program.cs
+++ LineComparisonProject/Program.cs
@@ -10,7 +10,9 @@ namespace LineComparisonProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparison project");
+            Console.WriteLine("Welcome to line comparison");
+            Console.WriteLine("\t\tEnter values for Line No 1: ");
+
             Console.WriteLine("Enter Value of : X1");
             //string value converted to integer
             int X1 = Convert.ToInt32(Console.ReadLine());
@@ -24,9 +26,44 @@ namespace LineComparisonProject
             Console.WriteLine("Enter Value of : Y2");
             int Y2 = Convert.ToInt32(Console.ReadLine());
 
+            //2st part line no 2 : L2
+            Console.WriteLine("\t\tEnter values for Line No 2: ");
+
+            Console.WriteLine("Enter Value of : X3");
+            int X3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : X4");
+            int X4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y3");
+            int Y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value of : Y4");
+            int Y4 = Convert.ToInt32(Console.ReadLine());
+
             //using double to get square root value in double decimal points
-            double length = Math.Sqrt((Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)));
-            Console.WriteLine("Length of the line is :" + length);
+
+            //to compare two lines we need 2 lengths as L1 and L2
+            double Length1 = Math.Sqrt((Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)));
+            Console.WriteLine("Length of the line is :" + Length1);
+
+            double Length2 = Math.Sqrt((Math.Pow((X4 - X3), 2) + Math.Pow((Y4 - Y3), 2)));
+            Console.WriteLine("Length of the line is :" + Length2);
+
+            //using sequence selection to check lines are equal or not 
+            if (Length1 == Length2)
+            {
+                Console.WriteLine("BOTH LINES ARE EQUAL");
+            }
+            else
+            {
+                Console.WriteLine("BOTH LINES ARE NOT EQUAL");
+            }
+
+            //using double to get square root value in double decimal points
+
+            double length1 = Math.Sqrt((Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)));
+            Console.WriteLine("Length of the line is :" + length1);
 
             Console.ReadLine();
         }
